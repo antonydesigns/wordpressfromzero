2f32022d1819a595e85e9b84009c901482d0f609
gio <gio>
2022-12-21 17:47:20 +0700

    simplified

--- template-parts/logo.php
+++ template-parts/logo.php
@@ -7,11 +7,11 @@
  */
 
 
-if (function_exists('the_custom_logo')) {
-    $id = get_theme_mod('custom_logo');
-    $logo = wp_get_attachment_image_src($id); // returns array[] of image data
-    $logo_src = $logo[0];
-} ?>
+
+$id = get_theme_mod('custom_logo');
+$logo = wp_get_attachment_image_src($id); // returns array[] of image data
+$logo_src = $logo[0];
+?>
 <div class="mid contains-logo">
     <a href="<?php echo esc_url(get_home_url()) ?>"><img src="<?php echo $logo_src ?>" class="logo gap" alt="the logo"></a>
 </div>
