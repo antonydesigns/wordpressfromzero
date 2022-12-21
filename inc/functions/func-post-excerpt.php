707b52b2a4f619adf36e5055cc62797a4dc06d9d
gio <gio>
2022-12-21 18:06:12 +0700

    simplifying again

--- inc/functions/func-post-excerpt.php
+++ inc/functions/func-post-excerpt.php
@@ -2,9 +2,8 @@
 
 function get_cryptoexplainer_excerpt($trim_character_count = null)
 {
-    $post_ID = get_the_ID();
 
-    $excerpt = wp_html_excerpt(get_the_excerpt($post_ID), $trim_character_count, '...');
+    $excerpt = wp_html_excerpt(get_the_excerpt(), $trim_character_count, '...');
 
     if (empty($post_ID)) {
         return null;
