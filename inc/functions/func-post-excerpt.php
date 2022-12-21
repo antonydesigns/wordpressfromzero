454cb7832bce8f2948c3623b5e3f19bbe7da0fef
gio <gio>
2022-12-21 18:13:56 +0700

    simplified functions

--- inc/functions/func-post-excerpt.php
+++ inc/functions/func-post-excerpt.php
@@ -2,17 +2,7 @@
 
 function get_cryptoexplainer_excerpt($trim_character_count = null)
 {
-
     $excerpt = wp_html_excerpt(get_the_excerpt(), $trim_character_count, '...');
-
-    if (empty($post_ID)) {
-        return null;
-    }
-
-    if (has_excerpt() || $trim_character_count = null) {
-
-        return $excerpt;
-    }
 
     return $excerpt;
 }
