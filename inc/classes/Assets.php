c2fa5a65555f91550bde516c72f1a8107ea9e155
gio <gio>
2023-01-07 12:39:29 +0700

    moved stylecss to assets folder

--- inc/classes/Assets.php
+++ inc/classes/Assets.php
@@ -29,7 +29,7 @@ class Assets
     public function register_styles()
     {
         wp_register_style('starter', get_template_directory_uri() . '/assets/starter.css', [], filemtime(get_template_directory() . '/assets/starter.css'), 'all');
-        wp_register_style('style', get_template_directory_uri() . '/style.css', [], filemtime(get_template_directory() . '/style.css'), 'all');
+        wp_register_style('style', get_template_directory_uri() . '/assets/style.css', [], filemtime(get_template_directory() . '/style.css'), 'all');
 
         wp_enqueue_style('starter');
         wp_enqueue_style('style');
